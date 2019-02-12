de61d0d76242a39fee353429a2df198dd4114e32
Hailey Somerville <hailey@hailey.lol>
2019-02-12 11:58:10 +1100

    move Mp3 to audio::decode::mp3

--- src/audio/decode.rs
+++ src/audio/decode.rs
@@ -1,4 +1,4 @@
-use std::io::{self, Read};
+use std::io;
 
 use super::PcmData;
 
@@ -13,32 +13,8 @@ pub trait PcmRead {
     fn read(&mut self) -> Result<PcmData, PcmReadError>;
 }
 
-pub struct Mp3<T: Read> {
-    mp3: minimp3::Decoder<T>,
-}
+mod mp3;
+pub use self::mp3::Mp3;
 
-impl<T: Read> Mp3<T> {
-    pub fn new(io: T) -> Self {
-        Mp3 { mp3: minimp3::Decoder::new(io) }
-    }
-}
-
-impl<T: Read> PcmRead for Mp3<T> {
-    fn read(&mut self) -> Result<PcmData, PcmReadError> {
-        match self.mp3.next_frame() {
-            Ok(frame) => Ok(PcmData {
-                sample_rate: frame.sample_rate as usize,
-                channels: frame.channels,
-                samples: frame.data.into_boxed_slice(),
-            }),
-            Err(minimp3::Error::Eof) => Err(PcmReadError::Eof),
-            Err(minimp3::Error::Io(e)) => Err(PcmReadError::Io(e)),
-            Err(minimp3::Error::SkippedData) => Err(PcmReadError::SkippedData),
-            Err(minimp3::Error::InsufficientData) => {
-                // InsufficientData is an error returned by decode_frame that
-                // is handled within next_frame. it should never appear here.
-                unreachable!()
-            }
-        }
-    }
-}
+mod ogg;
+pub use self::ogg::Ogg;
---
+++ src/audio/decode/mp3.rs
@@ -0,0 +1,34 @@
+use std::io::Read;
+
+use crate::audio::decode::{PcmRead, PcmReadError};
+use crate::audio::PcmData;
+
+pub struct Mp3<T: Read> {
+    mp3: minimp3::Decoder<T>,
+}
+
+impl<T: Read> Mp3<T> {
+    pub fn new(io: T) -> Self {
+        Mp3 { mp3: minimp3::Decoder::new(io) }
+    }
+}
+
+impl<T: Read> PcmRead for Mp3<T> {
+    fn read(&mut self) -> Result<PcmData, PcmReadError> {
+        match self.mp3.next_frame() {
+            Ok(frame) => Ok(PcmData {
+                sample_rate: frame.sample_rate as usize,
+                channels: frame.channels,
+                samples: frame.data.into_boxed_slice(),
+            }),
+            Err(minimp3::Error::Eof) => Err(PcmReadError::Eof),
+            Err(minimp3::Error::Io(e)) => Err(PcmReadError::Io(e)),
+            Err(minimp3::Error::SkippedData) => Err(PcmReadError::SkippedData),
+            Err(minimp3::Error::InsufficientData) => {
+                // InsufficientData is an error returned by decode_frame that
+                // is handled within next_frame. it should never appear here.
+                unreachable!()
+            }
+        }
+    }
+}
